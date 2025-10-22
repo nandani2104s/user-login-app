9a619a9f239f8753fb03211adcfd7801da309aeb
nandani2104 <sharannandani21@gmail.com>
2025-10-22 21:36:10 +0530

    models.py added

---
+++ models.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+    profile_picture = db.Column(db.String(200))
+    username = db.Column(db.String(100), unique=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    address = db.Column(db.String(200))
+    user_type = db.Column(db.String(50))  # 'Patient' or 'Doctor'
